614e32be46899f09c0b22030eff9f6f3cdea34b5
John R. D'Orazio <john.dorazio@cappellaniauniroma3.org>
2017-06-23 20:59:56 +0200

    increment microversion

--- themeswitcher.js
+++ themeswitcher.js
@@ -1,7 +1,7 @@
 /* jQuery plugin themeswitcher
 ---------------------------------------------------------------------*/
 (function($, undefined) {
-	$.themeswitcher = { "version":"2.0.36" };
+	$.themeswitcher = { "version":"2.0.37" };
 	Object.freeze($.themeswitcher);
 
 	$.fn.themeswitcher = function(settings){
