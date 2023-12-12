0ef99df0094d6a27221a9f74951b6b31fb8a0823
John R. D'Orazio <priest@johnromanodorazio.com>
2023-12-12 23:49:48 +0100

    prefer `on` event binding
    not that it makes much difference in this case, but for consistency and readability in style

--- themeswitcher.js
+++ themeswitcher.js
@@ -23,7 +23,7 @@
 				onClose: function(){},
 				onSelect: function(){}
 			}, settings);
-			
+
 			const availableUiVersions = ["1.13.2", "1.13.1", "1.13.0", "1.12.1", "1.12.0", "1.11.4", "1.11.3", "1.11.2", "1.11.1", "1.11.0", "1.10.4", "1.10.3", "1.10.2", "1.10.1", "1.10.0", "1.9.2", "1.9.1", "1.9.0", "1.8.24", "1.8.23", "1.8.22", "1.8.21", "1.8.20", "1.8.19", "1.8.18", "1.8.17", "1.8.16", "1.8.15", "1.8.14", "1.8.13", "1.8.12", "1.8.11", "1.8.10", "1.8.9", "1.8.8", "1.8.7", "1.8.6", "1.8.5", "1.8.4", "1.8.2", "1.8.1", "1.8.0", "1.7.3", "1.7.2", "1.7.1", "1.7.0", "1.6.0", "1.5.3", "1.5.2"];
 			if(false === availableUiVersions.includes(options.jqueryUiVersion)){
 				options.jqueryUiVersion = availableUiVersions[0];
@@ -142,11 +142,11 @@
 				let themeName		= themeLookupTable[theme].themeName;
 				let url			= getThemeUrl(theme);
 				let thumb		= getThumbUrl(theme);
-				let liOpenTag	= `<li><a href="${url}">`;
+				let liOpenTag		= `<li><a href="${url}">`;
 				let imgTag		= `<img src="${thumb}" alt="${themeName}" title="${themeName}" />`;
 				let span		= `<span class="themeName">${themeName}</span>`
-				let liCloseTag	= `</a></li>`;
-				let html = `${liOpenTag}${imgTag}${span}${liCloseTag}`;
+				let liCloseTag		= `</a></li>`;
+				let html		= `${liOpenTag}${imgTag}${span}${liCloseTag}`;
 				console.log(html);
 				return html;
 			}
@@ -168,7 +168,7 @@
 			    $switcherpane	= $(ul_string).find('div.themeGallery');
 
 			//button events
-			$button.click(() => {
+			$button.on('click', () => {
 				if($switcherpane.is(':visible')){
 					$button.button("option", "icon", "ui-icon-caret-1-s");
 					$button.removeClass("ui-corner-top").addClass("ui-corner-all");
@@ -189,7 +189,6 @@
 					left: $button.offset().left, 
 					width: $button.outerWidth() - Math.ceil( (window.innerWidth - $(window).width()) / 2 ) + 2 //this last 2 is to account for border-width, I think. seem to work...
 				}).slideDown(50); 
-				
 				options.onOpen(); 
 			}
 
@@ -200,7 +199,7 @@
 
 			/* Theme Loading
 			---------------------------------------------------------------------*/
-			$switcherpane.find('a').click(function() {
+			$switcherpane.find('a').on('click', function() {
 				updateCSS( $(this).attr('href') );
 				let themeName = $(this).find('span').text();
 				$button.button('option', 'label', options.buttonPreText + themeName);
